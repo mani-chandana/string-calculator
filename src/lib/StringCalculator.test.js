88226864a1898459dbf35acf5183ab983cf3e21f
mani chandana <nmcreddy2000@gmail.com>
2024-12-04 18:53:54 +0530

    test: add test for custom delimiters

--- src/lib/StringCalculator.test.js
+++ src/lib/StringCalculator.test.js
@@ -23,4 +23,8 @@ describe('StringCalculator', () => {
     expect(calculator.add("1\n2,3")).toBe(6);
   });
   
+  test('supports custom delimiters', () => {
+    expect(calculator.add("//;\n1;2")).toBe(3);
+  });
+  
 });
