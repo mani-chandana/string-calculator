1b31213616f18ca5810ced369220f8fe36d9adf6
mani chandana <nmcreddy2000@gmail.com>
2024-12-04 18:56:19 +0530

    feat: throw exception for negative numbers with details

--- src/lib/StringCalculator.js
+++ src/lib/StringCalculator.js
@@ -1,16 +1,20 @@
 export class StringCalculator {
     add(numbers) {
-      if (!numbers) return 0;
-  
-      let delimiter = /,|\n/;
-      if (numbers.startsWith("//")) {
-        const parts = numbers.split('\n');
-        delimiter = new RegExp(parts[0].substring(2));
-        numbers = parts[1];
-      }
-  
-      const parts = numbers.split(delimiter).map(num => parseInt(num, 10));
-      return parts.reduce((sum, num) => sum + num, 0);
+        if (!numbers) return 0;
+
+        let delimiter = /,|\n/;
+        if (numbers.startsWith("//")) {
+            const parts = numbers.split('\n');
+            delimiter = new RegExp(parts[0].substring(2));
+            numbers = parts[1];
+        }
+
+        const parts = numbers.split(delimiter).map(num => parseInt(num, 10));
+        const negatives = parts.filter(num => num < 0);
+        if (negatives.length) {
+            throw new Error(`Negatives not allowed: ${negatives.join(', ')}`);
+        }
+
+        return parts.reduce((sum, num) => sum + num, 0);
     }
-  }
-  
+}
