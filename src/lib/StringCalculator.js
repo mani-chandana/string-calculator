19c517bd2c92e2cd4b12c7993f72468711a4ffc5
mani chandana <nmcreddy2000@gmail.com>
2024-12-04 18:53:29 +0530

    feat: add support for newlines as delimiters

--- src/lib/StringCalculator.js
+++ src/lib/StringCalculator.js
@@ -2,7 +2,7 @@ export class StringCalculator {
     add(numbers) {
       if (!numbers) return 0;
   
-      const parts = numbers.split(',').map(num => parseInt(num, 10));
+      const parts = numbers.replace(/\n/g, ',').split(',').map(num => parseInt(num, 10));
       return parts.reduce((sum, num) => sum + num, 0);
     }
   }
