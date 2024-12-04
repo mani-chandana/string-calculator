4a760fe458010b8fd557ad22db566dedab1a4e2e
mani chandana <nmcreddy2000@gmail.com>
2024-12-04 19:11:10 +0530

    test: to have multiple delimiters

--- src/lib/StringCalculator.test.js
+++ src/lib/StringCalculator.test.js
@@ -45,4 +45,8 @@ describe('StringCalculator', () => {
     expect(calculator.add("//[***]\n1***2***3")).toBe(6);
   });
   
+  test('supports multiple custom delimiters', () => {
+    expect(calculator.add("//[*][%]\n1*2%3")).toBe(6);
+  });
+  
 });
